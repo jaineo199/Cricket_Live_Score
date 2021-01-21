4bb9bede1de60e2ae778fba1070391473fa1ab4b
jai <jai@jais-MacBook-Air.local>
2021-01-21 17:41:58 +0530

    Updates

---
+++ precache-manifest.920b00a4d7ea28d716e47bd7e115937c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e4745c13aa5019c26b30954e63fccaa",
+    "url": "/Cricket_Live_Score/index.html"
+  },
+  {
+    "revision": "892b38127029e450a6f4",
+    "url": "/Cricket_Live_Score/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "99d0adf05a0187eb8a25",
+    "url": "/Cricket_Live_Score/static/js/2.606eff13.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Cricket_Live_Score/static/js/2.606eff13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "892b38127029e450a6f4",
+    "url": "/Cricket_Live_Score/static/js/main.375decf1.chunk.js"
+  },
+  {
+    "revision": "56acf3dbc4154fd6a392",
+    "url": "/Cricket_Live_Score/static/js/runtime-main.e58a6b79.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Cricket_Live_Score/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "27b23a52df0c85503b9fe1b36595cb5a",
+    "url": "/Cricket_Live_Score/static/media/vs.27b23a52.png"
+  }
+]);
